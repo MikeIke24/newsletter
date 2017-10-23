8330b4ac144fd87d45feb6f31095fca1a32c7d2d
MikeIke24 <mnelms48@gmail.com>
2017-10-23 13:19:18 -0500

    stable base

--- src/app/tiles/tiles.component.ts
+++ src/app/tiles/tiles.component.ts
@@ -13,21 +13,33 @@ export class TilesComponent implements OnInit {
   el;
   btn;
   btnStyle;
+  header;
+  windowHeight;
   constructor(elementRef: ElementRef, renderer: Renderer2) { }
 
   ngOnInit() {
+    this.windowResize();
     this.btn = document.getElementById('shrink-button');
     this.btnStyle = this.btn.style;
+    this.header = document.getElementById('header');
   }
-
+  windowResize() {
+    this.windowHeight = window.innerHeight;
+    window.addEventListener('resize', () => {
+      this.windowHeight = window.innerHeight;
+      if (this.tileOpen) {
+        this.el.height = `${this.windowHeight}px`;
+      }
+    });
+  }
   expand(event) {
     if (this.allowExpand) {
     this.allowExpand = false;
     this.el = event.path[0].style;
     this.el.zIndex = '2';
     this.el.width = '100%';
-    this.el.height = '100vh';
-    this.el.top = '0%';
+    this.el.height = `${this.windowHeight}px`;
+    this.el.top = `-${this.header.offsetHeight}px`;
     this.el.left = '0%';
     this.tileOpen = true;
     this.btnStyle.right = '2rem';
@@ -41,6 +53,7 @@ export class TilesComponent implements OnInit {
     this.btnStyle.right = '-4rem';
     setTimeout(() => {
       this.el.zIndex = '0';
+      this.el = {};
       this.allowExpand = true;
     }, 500);
   }
